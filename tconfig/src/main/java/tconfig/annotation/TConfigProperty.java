f03c052216ba3da5f7e6e2372ee5ce73c4a9429c
Alexander Katanov <akatanov@roox.ru>
2016-09-27 14:07:51 +0300

    change defaultValue value from empty string to made up constant which contains unicode symbols. Not great solution, but Java  prohibits using nulls as default values.

--- tconfig/src/main/java/tconfig/annotation/TConfigProperty.java
+++ tconfig/src/main/java/tconfig/annotation/TConfigProperty.java
@@ -10,5 +10,15 @@ import java.lang.annotation.Target;
 public @interface TConfigProperty {
     String key();
 
-    String defaultValue() default "";
+    String defaultValue() default DEFAULT_NONE;
+
+    /**
+     * Constant defining a value for no default - as a replacement for
+     * {@code null} which we cannot use in annotation attributes.
+     * <p>This is an artificial arrangement of 16 unicode characters,
+     * with its sole purpose being to never match user-declared values.
+     * <p/>
+     * From spring-web =)
+     */
+    String DEFAULT_NONE = "\n\t\t\n\t\t\n\uE000\uE001\uE002\n\t\t\t\t\n";
 }
