b105c427875c62719bd9d63e2bf063ae2dea3c1b
Alexander Katanov <akatanov@roox.ru>
2016-09-27 14:52:00 +0300

    update javadoc

--- tconfig/src/main/java/tconfig/annotation/TConfigProperty.java
+++ tconfig/src/main/java/tconfig/annotation/TConfigProperty.java
@@ -17,8 +17,9 @@ public @interface TConfigProperty {
      * {@code null} which we cannot use in annotation attributes.
      * <p>This is an artificial arrangement of 16 unicode characters,
      * with its sole purpose being to never match user-declared values.
-     * <p/>
+     * <pre>
      * From spring-web =)
+     * </pre>
      */
     String DEFAULT_NONE = "\n\t\t\n\t\t\n\uE000\uE001\uE002\n\t\t\t\t\n";
 }
