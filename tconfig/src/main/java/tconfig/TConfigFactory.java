a92c1fa2dda902754fef7329ede8b20ce3291c25
Alexander Katanov <akatanov@roox.ru>
2016-09-27 14:08:39 +0300

    use AccessController in factory method.

--- tconfig/src/main/java/tconfig/TConfigFactory.java
+++ tconfig/src/main/java/tconfig/TConfigFactory.java
@@ -1,25 +1,38 @@
 package tconfig;
 
-import lombok.SneakyThrows;
-
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
 
 public class TConfigFactory {
     private static final String SUFFIX = "$tconfig";
 
-    @SneakyThrows({ClassNotFoundException.class, InstantiationException.class, IllegalAccessException.class, InvocationTargetException.class})
     public static <T> T createConfigFor(Class<T> type) {
-        final ClassLoader classLoader = type.getClassLoader();
-        final String typeName = type.getName();
-        Class<? extends T> configClass = Class.forName(typeName + SUFFIX, true, classLoader).asSubclass(type);
-        final Constructor<? extends T> constructor;
-        try {
-            constructor = configClass.getConstructor();
-        } catch (NoSuchMethodException e) {
-            throw new IllegalArgumentException("Class " + configClass + " has no matching constructor");
-        }
-        return constructor.newInstance();
+        return AccessController.doPrivileged(new PrivilegedAction<T>() {
+            @Override
+            public T run() {
+                final ClassLoader classLoader = type.getClassLoader();
+                final String typeName = type.getName();
+                Class<? extends T> configClass;
+                String configClassName = typeName + SUFFIX;
+                try {
+                    configClass = Class.forName(configClassName, true, classLoader).asSubclass(type);
+                } catch (ClassNotFoundException e) {
+                    throw new IllegalArgumentException(configClassName + " not found", e);
+                }
+                final Constructor<? extends T> constructor;
+                try {
+                    constructor = configClass.getConstructor();
+                } catch (NoSuchMethodException e) {
+                    throw new IllegalArgumentException("Class " + configClass + " has no matching constructor");
+                }
+                try {
+                    return constructor.newInstance();
+                } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+                    throw new IllegalArgumentException("Configuration implementation " + configClass + " could not be instantiated", e);
+                }
+            }
+        });
     }
-
 }
