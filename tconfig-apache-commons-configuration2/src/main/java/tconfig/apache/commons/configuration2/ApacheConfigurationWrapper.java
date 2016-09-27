3375fa71c4262868d3a95aba54818f588822d5e5
Alexander Katanov <akatanov@roox.ru>
2016-09-27 13:57:58 +0300

    add support for apache commons configuration2

---
+++ tconfig-apache-commons-configuration2/src/main/java/tconfig/apache/commons/configuration2/ApacheConfigurationWrapper.java
@@ -0,0 +1,67 @@
+package tconfig.apache.commons.configuration2;
+
+
+import tconfig.spi.Configuration;
+
+import java.util.List;
+import java.util.Objects;
+
+public class ApacheConfigurationWrapper implements Configuration {
+
+    private org.apache.commons.configuration2.Configuration delegate;
+
+    public ApacheConfigurationWrapper(org.apache.commons.configuration2.Configuration configuration) {
+        Objects.requireNonNull(configuration);
+        delegate = configuration;
+    }
+
+    @Override
+    public String getString(String key) {
+        return delegate.getString(key);
+    }
+
+    @Override
+    public String getString(String key, String defaultValue) {
+        return delegate.getString(key, defaultValue);
+    }
+
+    @Override
+    public int getInt(String key) {
+        return delegate.getInt(key);
+    }
+
+    @Override
+    public int getInt(String key, int defaultValue) {
+        return delegate.getInt(key, defaultValue);
+    }
+
+    @Override
+    public long getLong(String key) {
+        return delegate.getLong(key);
+    }
+
+    @Override
+    public long getLong(String key, long defaultValue) {
+        return delegate.getLong(key, defaultValue);
+    }
+
+    @Override
+    public boolean getBoolean(String key) {
+        return delegate.getBoolean(key);
+    }
+
+    @Override
+    public boolean getBoolean(String key, boolean defaultValue) {
+        return delegate.getBoolean(key, defaultValue);
+    }
+
+    @Override
+    public <T> List<T> getList(Class<T> cls, String key) {
+        return delegate.getList(cls, key);
+    }
+
+    @Override
+    public <T> List<T> getList(Class<T> cls, String key, List<T> defaultValue) {
+        return delegate.getList(cls, key, defaultValue);
+    }
+}
