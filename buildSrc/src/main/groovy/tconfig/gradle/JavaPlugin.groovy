692369d236fa65fa22dd7fd9920f79bfd00e63a2
Alexander Katanov <akatanov@roox.ru>
2016-09-30 15:21:37 +0300

    add some build info into jar MANIFEST

--- buildSrc/src/main/groovy/tconfig/gradle/JavaPlugin.groovy
+++ buildSrc/src/main/groovy/tconfig/gradle/JavaPlugin.groovy
@@ -9,6 +9,24 @@ public class JavaPlugin implements Plugin<Project> {
     @Override
     public void apply(Project project) {
         project.apply({ it.plugin(org.gradle.api.plugins.JavaPlugin.class) })
+
+        project.jar {
+            manifest {
+                attributes(
+                        "Archiver-Version": "Gradle ${project.gradle.gradleVersion}",
+                        "Created-By": "Gradle",
+                        "Built-By": System.getProperty('user.name'),
+                        "Built-Jdk": System.getProperty('java.version'),
+                        "X-Build-Tag": System.getenv("TRAVIS_TAG") ?: "DEVELOPER_BUILD",
+                        "X-Build-Number": project.version.build ?: "SNAPSHOT",
+                        "X-Build-Id": System.getenv("TRAVIS_BUILD_ID") ?: "DEVELOPER_BUILD",
+                        "X-Commit": System.getenv("TRAVIS_COMMIT") ?: "DEVELOPER_BUILD",
+                        "X-Branch": System.getenv("TRAVIS_BRANCH") ?: "DEVELOPER_BUILD",
+                        "X-Component-Id": project.name,
+                        "X-Component-Version": version,
+                )
+            }
+        }
     }
 
 }
